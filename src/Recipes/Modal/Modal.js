0e99f24d08fc6ca574c60fe446c58aa9541c71c3
Simone Ghergo <simone.ghergo.external@ynap.com>
2019-11-26 17:01:27 +0100

    Added Recipe folder + Modal + fixed React Fragment

---
+++ src/Recipes/Modal/Modal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Modal.propTypes = {};
+
+export default function Modal(props) {
+    return (<React.Fragment>Modal</React.Fragment>);
+}
---
+++ src/Recipes/Modal/__tests__/Modal.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Modal from '..';
+
+export default {
+    title: '',
+    component: Modal
+};
+
+export const Modal = () => {
+	return(<React.Fragment>Modal</React.Fragment>)
+};
+
+Modal.story = {
+    name: ''
+};
