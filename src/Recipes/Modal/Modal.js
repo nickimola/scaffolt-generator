9e530fef88e4a7d80fc88badd0d5b0728af11756
Simone Ghergo <simone.ghergo.external@ynap.com>
2019-11-26 17:20:24 +0100

    Removed src folder

--- src/Recipes/Modal/Modal.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-Modal.propTypes = {};
-
-export default function Modal(props) {
-    return (<React.Fragment>Modal</React.Fragment>);
-}
